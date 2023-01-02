c1c9319c566e8808dbb21b6c89e93edb0f7d578c
Sandip-Kumar-Roy <101457128+sandip-kumar-roy@users.noreply.github.com>
2023-01-02 18:43:08 +0530

    Delete FINAL YEAR PROJECT MAIN/heart_disease_prediction directory

--- FINAL YEAR PROJECT MAIN/heart_disease_prediction/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from accounts.models import UserProfileInfo
-
-class UserProfile(admin.ModelAdmin):
-    list_display = ('user', 'profile_pic')
-admin.site.register(UserProfileInfo,UserProfile)
-# Register your models here.
--- FINAL YEAR PROJECT MAIN/heart_disease_prediction/predict_risk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PredictRiskConfig(AppConfig):
-    name = 'predict_risk'
