8b1b3b723be4dc8ac6ca914666d6fdc20495afaf
Sandip-Kumar-Roy <101457128+sandip-kumar-roy@users.noreply.github.com>
2023-01-05 16:36:29 +0530

    Delete contact directory

--- contact/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from contact.models import contact_form
-# Register your models here.
-class contact_formAdmin(admin.ModelAdmin):
-    list_display = ["firstnm","lastnm","mail","contact","mesg"]
-admin.site.register(contact_form,contact_formAdmin)
-
--- contact/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
-from . import views
-urlpatterns = [
-    path('contact_us', views.contact_us,name='contact_us'),
-    
-    
-]
