03e534c365971dc8aa17f255329d6ad489452fa5
Sandip-Kumar-Roy <101457128+sandip-kumar-roy@users.noreply.github.com>
2023-01-05 16:36:42 +0530

    Delete editprofile directory

--- editprofile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
-from . import views
-urlpatterns = [
-    path('profile_base', views.profile_base,name='profile_base'),
-    path('edit_profile', views.edit_profile,name='edit_profile'),
-
-    path('change_pass', views.edit_profile_password,name='change_pass'),
-]
